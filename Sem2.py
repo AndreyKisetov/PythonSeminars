ec00a40f7cdc64bd02b4fad23a0f1045a7d6eea7
Andrey Kisetov <rbcfff@yandex.ru>
2022-09-06 04:20:18 +0300

    added tascs in Sem2.py

---
+++ Sem2.py
@@ -0,0 +1,24 @@
+# Найти индексы зражённых вирусом "a n t o n" холодильников.
+
+# x = int(input())
+# list = []
+# for i in range(x):
+#     a = input()
+#     if 'a' in a:
+#         a = a[a.find('a'):]
+#         if 'n' in a:
+#             a = a[a.find('n'):]
+#             if 't' in a:
+#                 a = a[a.find('t'):]
+#                 if 'o' in a:
+#                     a = a[a.find('o'):]
+#                     if 'n' in a:
+#                         list.append(i + 1)
+# print(*list)
+  
+# определяем количество вхождений одной строки в другой.
+
+# x = 'типа перерабатываемого материала'
+# y = 'ер'
+# print(x.count(y))
+
